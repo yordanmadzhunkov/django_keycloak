49e34e02a35af232a00a3cc42ef4516302a4dba9
Yordan Madzhunkov <ymadzhunkov@gmail.com>
2023-09-11 11:46:07 +0300

    Image building script with secrets

---
+++ build_it.sh
@@ -0,0 +1,18 @@
+python -c ""
+
+echo "# automaticaly generated on $(date)" > .env
+echo "" >> .env
+
+# for older python
+# SECRET=$(python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
+
+#for Python 3.6+
+SECRET=$(python -c 'import secrets; print(secrets.token_urlsafe())')
+echo "DJANGO_SECRET_KEY=\"${SECRET}\"" >> .env
+
+DJANGO_GIT_TAG=$(git describe --tags --dirty --long)
+echo "DJANGO_GIT_TAG=\"${DJANGO_GIT_TAG}\"" >> .env
+cat oidc.env >> .env
+
+echo "Starting build .. "
+docker compose build show_users
--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd $APP_PATH
-
-gunicorn --bind 0.0.0.0:8000 show_users.wsgi:application
