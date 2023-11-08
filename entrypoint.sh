42c7bf9171a7f32dfa41487603543b602f7556bb
Yordan Madzhunkov <ymadzhunkov@gmail.com>
2023-11-08 15:19:58 +0000

    Start demo platform

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 ./manage.py collectstatic --noinput
 # i commit my migration files to git so i dont need to run it on server
-# ./manage.py makemigrations show_users
 ./manage.py migrate
 
-gunicorn --bind :8000 --workers ${NUM_WORKERS} show_users.wsgi:application
+gunicorn --bind :8000 --workers ${NUM_WORKERS} vei_platform.wsgi:application
