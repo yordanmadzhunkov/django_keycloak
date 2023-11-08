6cca749b846010d41a736486c67fdf83ebbe1221
Yordan Madzhunkov <ymadzhunkov@gmail.com>
2023-11-08 15:35:51 +0000

    Try this settings

--- apps/vei_platform/settings/production.py
+++ apps/vei_platform/settings/production.py
@@ -1,39 +1,30 @@
 from .base import *
 from decouple import config
+from pathlib import Path
 
 DEBUG = config("DEBUG", default=False, cast=bool)
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY              = config('DJANGO_SECRET_KEY')
+
+ALLOWED_HOSTS           = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')], default='*')
+CSRF_TRUSTED_ORIGINS    = config('CSRF_TRUSTED_ORIGINS', cast=lambda v: [s.strip() for s in v.split(',')], default='*')
+CORS_ORIGIN_WHITELIST   = config('CORS_ORIGIN_WHITELIST', cast=lambda v: [s.strip() for s in v.split(',')], default='*')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+TIME_ZONE = 'UTC'
 
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DB'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': config('POSTGRES_HOST'),
-        'PORT': config('POSTGRES_PORT'),
+        'NAME': config('DJANGO_POSTGRES_DB'),
+        'USER': config('DJANGO_POSTGRES_USER'),
+        'PASSWORD': config('DJANGO_POSTGRES_PASSWORD'),
+        'HOST': config('DJANGO_POSTGRES_HOST'),
+        'PORT': config('DJANGO_POSTGRES_PORT'),
     }
 }
 
-TIME_ZONE = 'UTC'
 
-SECRET_KEY = config('SECRET_KEY')
-SESSION_COOKIE_SECURE = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_SECONDS = 31536000
-SECURE_REDIRECT_EXEMPT = []
-SECURE_SSL_REDIRECT = False  # We are using nginx to handle your HTTPS redirects
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CSRF_TRUSTED_ORIGINS = ['https://vei-platform', 'https://127.0.0.1']
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
