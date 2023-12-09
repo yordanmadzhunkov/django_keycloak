d103498508b5600bfe9cebd5f2b3ccd50fcfcf3d
Yordan Madzhunkov <ymadzhunkov@gmail.com>
2023-12-09 12:52:47 +0000

    Add running tests in docker container

--- build_it.sh
+++ build_it.sh
@@ -16,5 +16,18 @@ DJANGO_GIT_TAG=$(git describe --tags --dirty --long)
 echo "DJANGO_GIT_TAG=\"${DJANGO_GIT_TAG}\"" >> .env
 cat oidc.env >> .env
 
+
 echo "Starting build .. "
 docker compose build vei_platform
+
+
+
+echo "# automaticaly generated on $(date)" > .env_test
+echo "" >> .env_test
+SECRET=$(python -c 'import secrets; print(secrets.token_urlsafe())')
+echo "DJANGO_SECRET_KEY=\"${SECRET}\"" >> .env_test
+echo 'DJANGO_SETTINGS_MODULE="vei_platform.settings.tests"' >> .env_test
+echo "DJANGO_GIT_TAG=\"${DJANGO_GIT_TAG}\"" >> .env_test
+cat oidc.env >> .env_test
+
+docker compose run vei_platform_test
