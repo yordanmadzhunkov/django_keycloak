276ce111bc79080fe8129791bc14b00312e09554
Yordan Madzhunkov <ymadzhunkov@gmail.com>
2023-12-29 14:25:33 +0200

    Fix dashboard bug

--- apps/vei_platform/views/dashboard.py
+++ apps/vei_platform/views/dashboard.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from . import common_context
@@ -10,17 +10,19 @@ from vei_platform.models.factory import ElectricityFactory
 def view_dashboard(request):
     context = common_context(request)
     if request.user:
-        context['my_legal_entity'] = find_legal_entity(user=request.user)
         if request.user.is_authenticated:
+            context['my_legal_entity'] = find_legal_entity(user=request.user)
             profile = get_user_profile(request.user)
             my_investments = InvestementInCampaign.objects.filter(investor_profile=profile)
             context['my_investments'] = my_investments
 
-        factories_list = ElectricityFactory.objects.filter(manager=request.user).order_by('pk')
-        campaigns = []
-        for factory in factories_list:
-            factory_campaigns = Campaign.objects.filter(factory=factory)
-            for c in factory_campaigns:
-                campaigns.append(c)
-        context['campaigns'] = campaigns
+            factories_list = ElectricityFactory.objects.filter(manager=request.user).order_by('pk')
+            campaigns = []
+            for factory in factories_list:
+                factory_campaigns = Campaign.objects.filter(factory=factory)
+                for c in factory_campaigns:
+                    campaigns.append(c)
+            context['campaigns'] = campaigns
+        else:
+            return redirect('home')
     return render(request, "dashboard.html", context)
